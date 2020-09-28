68a601de39d5edd3af157350a41a670730e112e1
Alonso Perez <jesus.perez3854@alumnos.udg.mx>
2020-09-28 17:40:22 -0500

    Actividad 7 (Sizeof)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string tipoDato;
 
+    getline(cin, tipoDato);
+    if(tipoDato == "char"){
+        cout << sizeof(char) << endl;
+    }else if (tipoDato == "int"){
+        cout << sizeof(int) << endl;
+    }else if(tipoDato == "short"){
+        cout << sizeof(short) << endl;
+    }else if (tipoDato == "float"){
+        cout << sizeof(float) << endl;
+    }else if (tipoDato == "double"){
+        cout << sizeof(double) << endl;
+    }else if (tipoDato == "long"){
+        cout << sizeof(long) << endl;
+    }
+    
     return 0;
 }
